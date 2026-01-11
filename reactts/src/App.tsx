fcd8019dd8a4dae1a67345c5b7240e896883555c
Ishwar Suthar <ishwar16suthar@gmail.com>
2026-01-11 05:40:15 +0530

    setup react with typescript

---
+++ reactts/src/App.tsx
@@ -0,0 +1,40 @@
+import { ChaiCard } from "./components/ChaiCard.tsx";
+import ChaiList from "./components/ChaiList.tsx";
+import { Counter } from "./components/Counter.tsx";
+import { OrderForm } from "./components/OrderForm.tsx";
+import type { Chai } from "./types.ts";
+
+const menu: Chai[] = [
+  { id: 1, name: "Masala Chai", price: 20 },
+  { id: 2, name: "Ginger Chai", price: 40 },
+  { id: 3, name: "Lemon Chai", price: 50 },
+];
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div>
+          <h1>Vite + React</h1>
+          <ChaiCard name="Masala Chai" price={20} />
+          <ChaiCard name="Ginger Chai" price={30} />
+        </div>
+        <div>
+          <Counter />
+        </div>
+        <div>
+          <ChaiList items={menu} />
+        </div>
+        <div>
+          <OrderForm
+            onSubmit={(order) => {
+              console.log("Placed", order.name, order.cups);
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
